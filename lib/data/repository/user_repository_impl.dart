50183f8878273b39047f7c83278821dba6a85944
Prasetya Adi Arfanto <prasetyaarfanto@gmail.com>
2023-05-29 20:58:58 +0700

    Add user repository implementation

---
+++ lib/data/repository/user_repository_impl.dart
@@ -0,0 +1,22 @@
+import 'package:chopper/src/response.dart';
+import 'package:my_reqres_login/data/source/network/model/request/login_request.dart';
+import 'package:my_reqres_login/data/source/network/model/response/login_response.dart';
+import 'package:my_reqres_login/data/source/network/model/response/user_response.dart';
+import 'package:my_reqres_login/data/source/network/service/login_service.dart';
+import 'package:my_reqres_login/data/source/network/service/user_service.dart';
+import 'package:my_reqres_login/domain/repository/user_repository.dart';
+
+class UserRepositoryImpl implements UserRepository {
+  final LoginService _loginService;
+  final UserService _userService;
+
+  UserRepositoryImpl(this._loginService, this._userService);
+
+  @override
+  Future<Response<UserResponse>> getUser(int userId) async =>
+      await _userService.getUser(userId);
+
+  @override
+  Future<Response<LoginResponse>> login(LoginRequest request) async =>
+      await _loginService.login(request);
+}
