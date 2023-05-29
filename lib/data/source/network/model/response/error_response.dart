3aa6fd1be2be5f2b60af4433110c0ad10f52e421
Prasetya Adi Arfanto <prasetyaarfanto@gmail.com>
2023-05-29 20:45:36 +0700

    Add error response

---
+++ lib/data/source/network/model/response/error_response.dart
@@ -0,0 +1,15 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'error_response.g.dart';
+
+@JsonSerializable()
+class ErrorResponse {
+  final String error;
+
+  ErrorResponse(this.error);
+
+  factory ErrorResponse.fromJson(Map<String, dynamic> json) =>
+      _$ErrorResponseFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ErrorResponseToJson(this);
+}
