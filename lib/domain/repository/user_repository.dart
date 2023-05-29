fffd12bd741f40e595cf57a5458a655df1f0a942
Prasetya Adi Arfanto <prasetyaarfanto@gmail.com>
2023-05-29 20:57:01 +0700

    Add abstract user repository

---
+++ lib/domain/repository/user_repository.dart
@@ -0,0 +1,10 @@
+import 'package:chopper/chopper.dart';
+import 'package:my_reqres_login/data/source/network/model/request/login_request.dart';
+import 'package:my_reqres_login/data/source/network/model/response/login_response.dart';
+import 'package:my_reqres_login/data/source/network/model/response/user_response.dart';
+
+abstract class UserRepository {
+  Future<Response<LoginResponse>> login(LoginRequest request);
+
+  Future<Response<UserResponse>> getUser(int userId);
+}
